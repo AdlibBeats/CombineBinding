2b04964c6dcafa071fbab36f2c36a8cf0f163547
Андрей Васильев <avasilyev@mobiledimension.ru>
2020-08-08 19:49:44 +0300

    add extensions

--- CombineBinding/Classes/CombineExtensions.swift
+++ CombineBinding/Classes/CombineExtensions.swift
@@ -39,10 +39,9 @@ public extension Combine where Base: UIViewController {
 
 public extension Combine where Base: UIButton {
     func setTitle(for state: UIControl.State) -> ((String?) -> Void) { { [weak base] in base?.setTitle($0, for: state) } }
-}
-
-public extension Combine where Base: UILabel {
-    var text: ((String?) -> Void) { { [weak base] in base?.text = $0 } }
+    func setTitleColor(for state: UIControl.State) -> ((UIColor?) -> Void) { { [weak base] in base?.setTitleColor($0, for: state) } }
+    func setImage(for state: UIControl.State) -> ((UIImage?) -> Void) { { [weak base] in base?.setImage($0, for: state) } }
+    func setBackgroundImage(for state: UIControl.State) -> ((UIImage?) -> Void) { { [weak base] in base?.setBackgroundImage($0, for: state) } }
 }
 
 public extension Combine where Base: UIDatePicker {
@@ -50,4 +49,48 @@ public extension Combine where Base: UIDatePicker {
     var minimumDate: ((Date?) -> Void) { { [weak base] in base?.minimumDate = $0 } }
     var maximumDate: ((Date?) -> Void) { { [weak base] in base?.maximumDate = $0 } }
     func setDate(animated: Bool) -> ((Date) -> Void) { { [weak base] in base?.setDate($0, animated: animated) } }
+    var datePickerMode: ((UIDatePicker.Mode) -> Void) { { [weak base] in base?.datePickerMode = $0 } }
+}
+
+public extension Combine where Base: UITextField {
+    var text: ((String?) -> Void) { { [weak base] in base?.text = $0 } }
+    var textColor: ((UIColor?) -> Void) { { [weak base] in base?.textColor = $0 } }
+    var textAlignment: ((NSTextAlignment) -> Void) { { [weak base] in base?.textAlignment = $0 } }
+    var font: ((UIFont) -> Void) { { [weak base] in base?.font = $0 } }
+}
+
+public extension Combine where Base: UITextView {
+    var text: ((String?) -> Void) { { [weak base] in base?.text = $0 } }
+    var textColor: ((UIColor?) -> Void) { { [weak base] in base?.textColor = $0 } }
+    var textAlignment: ((NSTextAlignment) -> Void) { { [weak base] in base?.textAlignment = $0 } }
+    var font: ((UIFont?) -> Void) { { [weak base] in base?.font = $0 } }
+}
+
+public extension Combine where Base: UILabel {
+    var text: ((String?) -> Void) { { [weak base] in base?.text = $0 } }
+    var textColor: ((UIColor?) -> Void) { { [weak base] in base?.textColor = $0 } }
+    var textAlignment: ((NSTextAlignment) -> Void) { { [weak base] in base?.textAlignment = $0 } }
+    var font: ((UIFont) -> Void) { { [weak base] in base?.font = $0 } }
+    var lineBreakMode: ((NSLineBreakMode) -> Void) { { [weak base] in base?.lineBreakMode = $0 } }
+    var numberOfLines: ((Int) -> Void) { { [weak base] in base?.numberOfLines = $0 } }
+}
+
+public extension Combine where Base: CALayer {
+    var cornerRadius: ((CGFloat) -> Void) { { [weak base] in base?.cornerRadius = $0 } }
+}
+
+public extension Combine where Base: UIControl {
+    var isEnabled: ((Bool) -> Void) { { [weak base] in base?.isEnabled = $0 } }
+    var isSelected: ((Bool) -> Void) { { [weak base] in base?.isSelected = $0 } }
+    var isHighlighted: ((Bool) -> Void) { { [weak base] in base?.isHighlighted = $0 } }
+}
+
+public extension Combine where Base: UIControl {
+    var isUserInteractionEnabled: ((Bool) -> Void) { { [weak base] in base?.isUserInteractionEnabled = $0 } }
+}
+
+public extension Combine where Base: UIView {
+    var isHidden: ((Bool) -> Void) { { [weak base] in base?.isHidden = $0 } }
+    var backgroundColor: ((UIColor?) -> Void) { { [weak base] in base?.backgroundColor = $0 } }
+    var tintColor: ((UIColor?) -> Void) { { [weak base] in base?.tintColor = $0 } }
 }
